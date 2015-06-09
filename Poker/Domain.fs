75c3b3b7972c021549e25488fc23293fb771457f
Max Malook <community@malook.de>
2015-06-09 14:18:11 +0200

    add Poker

---
+++ Poker/Domain.fs
@@ -0,0 +1,52 @@
+﻿namespace Poker.Types
+
+type Suit =
+    | Clubs
+    | Hearts
+    | Spades
+    | Diamonds
+
+type Rank =
+    | Ace
+    | King
+    | Queen
+    | Jack
+    | Ten
+    | Nine
+    | Eight
+    | Seven
+    | Six
+    | Five
+    | Four
+    | Three
+    | Two
+
+type Card =
+    { Rank: Rank
+      Suit: Suit }
+
+type Hand = Card list
+
+type HandRank =
+    | RoyalFlush
+    | StraightFlush of Rank
+    | FourOfAKind of Rank * kicker: Rank
+    | FullHouse of Rank * over: Rank
+    | Flush of ranks: (Rank * Rank * Rank * Rank * Rank)
+    | Straight of Rank
+    | ThreeOfAKind of Rank * kickers: (Rank * Rank)
+    | TwoPair of Rank * over: Rank * kicker: Rank
+    | OnePair of Rank * kickers: (Rank * Rank * Rank)
+    | High of Rank * kickers: (Rank * Rank * Rank * Rank)
+
+type Classifier = Hand -> HandRank
+
+type Player =
+  { Name: string
+    Hand: Hand }
+
+type Result =
+    | Winner of Player
+    | SplitPot of Player list
+
+type Game = Player list -> Result
---
+++ Poker/Game.fs
@@ -0,0 +1,89 @@
+﻿module Poker.Game
+
+open Poker.Types
+
+let rank = fst
+let suit = snd
+
+let (|Sorted|) xs = xs |> Seq.sort |> List.ofSeq
+
+let (|RankGroups|) hand =
+    hand |> (Seq.groupBy id >> Seq.map (snd >> List.ofSeq) >> Seq.sortBy (Seq.length >> (~-)) >> List.ofSeq)
+
+let forall p list =
+    list
+    |> Seq.pairwise
+    |> Seq.forall p
+    |> (fun value -> if value then Some() else None)
+    |> Option.map (fun () -> list |> Seq.head)
+
+let (|Tag|) (a: 't) =
+    Microsoft.FSharp.Reflection.FSharpValue.PreComputeUnionTagReader(typeof<'t>) (box a)
+
+let (|StraightInHand|_|) =
+    forall (fun (Tag a, Tag b) -> b - a = 1) >> Option.map Seq.head
+
+let (|OfSameSuit|_|) =
+    forall (fun (a, b) -> a = b)
+
+let (|NumberOfAKind|_|) n group =
+    if group |> Seq.length = n then
+        group |> Seq.head |> Some
+    else None
+
+let (|FiveKickers|_|) groups =
+    match groups with
+    | [[kicker1]; [kicker2]; [kicker3]; [kicker4]; [kicker5]] -> (kicker1, kicker2, kicker3, kicker4, kicker5) |> Some
+    | _ -> None
+
+let (|FourKickers|_|) groups =
+    match groups with
+    | [[kicker1]; [kicker2]; [kicker3]; [kicker4]] -> (kicker1, kicker2, kicker3, kicker4) |> Some
+    | _ -> None
+
+let (|ThreeKickers|_|) groups =
+    match groups with
+    | [[kicker1]; [kicker2]; [kicker3]] -> (kicker1, kicker2, kicker3) |> Some
+    | _ -> None
+
+let (|TwoKickers|_|) groups =
+    match groups with
+    | [[kicker1]; [kicker2]] -> (kicker1, kicker2) |> Some
+    | _ -> None
+
+let (|OneKicker|_|) groups =
+    match groups with
+    | [[kicker1]] -> kicker1 |> Some
+    | _ -> None
+
+let detect (Sorted hand) =
+    let RankGroups rankGroups, suits = hand |> List.unzip
+    match rankGroups, suits with
+    | StraightInHand rank                                               , OfSameSuit suit   -> StraightFlush (rank)
+    | NumberOfAKind 4 rank :: OneKicker kicker                          , _                 -> FourOfAKind (rank, kicker)
+    | NumberOfAKind 3 rank :: NumberOfAKind 2 over :: []                , _                 -> FullHouse (rank, over)
+    | FiveKickers kickers                                               , OfSameSuit suit   -> Flush (kickers)
+    | StraightInHand rank                                               , _                 -> Straight rank
+    | NumberOfAKind 3 rank :: TwoKickers kickers                        , _                 -> ThreeOfAKind (rank, kickers)
+    | NumberOfAKind 2 rank :: NumberOfAKind 2 over :: OneKicker kicker  , _                 -> TwoPair (rank, over, kicker)
+    | NumberOfAKind 2 rank :: ThreeKickers kickers                      , _                 -> OnePair (rank, kickers)
+    | NumberOfAKind 1 rank :: FourKickers kickers                       , _                 -> High (rank, kickers)
+    | _                                                                 , _                 -> failwithf "unknown hand %A" hand
+
+let handClassifier : Classifier =
+    fun hand ->
+        hand
+        |> Seq.map (fun c -> c.Rank, c.Suit)
+        |> detect
+
+let game : Game =
+    fun players ->
+        let _, topHandRank =
+            players
+            |> Seq.groupBy (fun p -> p.Hand |> handClassifier)
+            |> Seq.sortBy fst
+            |> Seq.head
+
+        match topHandRank |> Seq.toList with
+        | [winner] -> Winner winner
+        | players -> SplitPot players
